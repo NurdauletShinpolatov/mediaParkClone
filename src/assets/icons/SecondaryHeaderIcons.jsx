1f0063f0cce62369b3fccd174974b4bb4874d69d
Nurdaulet Shinpolatov <2110017@172-10-61-215.lightspeed.moblal.sbcglobal.net>
2024-07-11 17:47:23 +0500

    working on SecondaryHeader

--- src/assets/icons/SecondaryHeaderIcons.jsx
+++ src/assets/icons/SecondaryHeaderIcons.jsx
@@ -34,9 +34,12 @@ export function ArrowIcon() {
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      style={{ fill: "rgba(0, 0, 0, 1)", rotate: '45deg'}}
+      style={{ fill: "rgba(255, 255, 255, 1)", rotate: '45deg'}}
     >
-      <path d="M11 8.414V18h2V8.414l4.293 4.293 1.414-1.414L12 4.586l-6.707 6.707 1.414 1.414z"></path>
+      <path 
+        d="M11 8.414V18h2V8.414l4.293 4.293 1.414-1.414L12 4.586l-6.707 6.707 1.414 1.414z"
+      >
+      </path>
     </svg>
   );
 }
--- src/components/SecondaryHeader/index.jsx
+++ src/components/SecondaryHeader/index.jsx
@@ -1,19 +1,36 @@
-'use client'
-import { Box, Flex, Select, Text } from '@chakra-ui/react';
+"use client"
+
+import { Flex, Text } from '@chakra-ui/react';
 import cls from './SecondaryHeader.module.scss';
 import { ArrowIcon, LocationIcon, PhoneIcon } from '@/assets/icons/SecondaryHeaderIcons';
 import { useState } from 'react';
+import Select from 'react-select';
+
 
 const languages = {uz: "O'zbek", ru: "русский", en: "English"}
 
 export default function SecondaryHeader() {
   const [lang, setLang] = useState('uz')
 
-  const changeLanguage = (v) => {
-    console.log("v", v) // log
+  const changeLanguage = (option) => {
+    setLang(option.value)
   }
 
-  const currentLanguage = languages?.[lang]
+  const langOptions = [
+    {
+      value: 'uz',
+      label: "O'zbek",
+    },
+    {
+      value: 'ru',
+      label: "Русский",
+    },
+    {
+      value: 'en',
+      label: 'English'
+    }
+  ]
+
   return(
     <div className={cls.secondaryHeader}>
       <Flex className={cls.container} px={15} py={10} justifyContent='space-between' >
@@ -29,18 +46,26 @@ export default function SecondaryHeader() {
           </Flex>
         </Flex>
         
-        <Flex>
+        <Flex gap='24px' fontSize='12px'>
           <Text>Biz haqimizda</Text>
           <div className={cls.roundedRedBtn}>MediaClub</div>
           <div className={cls.roundedBlueBtn}>MediaBlog <ArrowIcon /></div>
           <Text>Yuridik shaxslar uchun</Text>
           <Text>Do'konlar</Text>
           <Text>Biz bilan bog'laning!</Text>
-          <Select variant='flushed' placeholder={currentLanguage} value={lang} onChange={changeLanguage}>
+          {/* <Select variant='flushed' placeholder={currentLanguage} value={lang} onChange={changeLanguage}>
             <option value="uz">O'zbek</option>
             <option value="ru">Руский</option>
             <option value="en">English</option>
-          </Select>
+          </Select> */}
+          <Select
+            className="basic-single"
+            classNamePrefix="select"
+            defaultValue={langOptions[0]}
+            name="language"
+            options={langOptions}
+            onChange={changeLanguage}
+          />
         </Flex>
       </Flex>
     </div>
