163d1447d238859aab48c8325ebb08b45f1a432f
NurdauletShinpolatov <nshinpolatov@gmail.com>
2024-07-12 16:28:33 +0500

    working on SecondaryHeader

--- src/components/SecondaryHeader/index.jsx
+++ src/components/SecondaryHeader/index.jsx
@@ -1,73 +1,84 @@
-"use client"
+"use client";
 
-import { Flex, Text } from '@chakra-ui/react';
-import cls from './SecondaryHeader.module.scss';
-import { ArrowIcon, LocationIcon, PhoneIcon } from '@/assets/icons/SecondaryHeaderIcons';
-import { useState } from 'react';
-import Select from 'react-select';
+import { Flex, Select, Text } from "@chakra-ui/react";
+import cls from "./SecondaryHeader.module.scss";
+import {
+  ArrowIcon,
+  LocationIcon,
+  PhoneIcon,
+} from "@/assets/icons/SecondaryHeaderIcons";
+import { useState } from "react";
 
-
-const languages = {uz: "O'zbek", ru: "русский", en: "English"}
+// const languages = {uz: "O'zbek", ru: "русский", en: "English"}
 
 export default function SecondaryHeader() {
   const [lang, setLang] = useState('uz')
 
-  const changeLanguage = (option) => {
-    setLang(option.value)
-  }
+  const changeLanguage = (e) => {
+    console.log("e", e); // log
+    setLang(e.target.value);
+  };
 
   const langOptions = [
     {
-      value: 'uz',
+      value: "uz",
       label: "O'zbek",
     },
     {
-      value: 'ru',
+      value: "ru",
       label: "Русский",
     },
     {
-      value: 'en',
-      label: 'English'
-    }
-  ]
+      value: "en",
+      label: "English",
+    },
+  ];
 
-  return(
+  return (
     <div className={cls.secondaryHeader}>
-      <Flex className={cls.container} px={15} py={10} justifyContent='space-between' >
-        
-        <Flex gap='24px'>
-          <Flex alignItems='center' gap='4px'>
+      <Flex
+        className={cls.container}
+        px={15}
+        py={10}
+        justifyContent="space-between"
+      >
+        <Flex gap="24px">
+          <Flex alignItems="center" gap="4px">
             <PhoneIcon />
-            <Text fontSize='12px'>+998 71 203 33 33</Text>
+            <Text fontSize="12px">+998 71 203 33 33</Text>
           </Flex>
-          <Flex alignItems='center'>
+          <Flex alignItems="center">
             <LocationIcon />
-            <Text fontSize='12px'>Toshkent</Text>
+            <Text fontSize="12px">Toshkent</Text>
           </Flex>
         </Flex>
-        
-        <Flex gap='24px' fontSize='12px'>
+
+        <Flex gap="24px" fontSize="12px" alignItems='center'>
           <Text>Biz haqimizda</Text>
           <div className={cls.roundedRedBtn}>MediaClub</div>
-          <div className={cls.roundedBlueBtn}>MediaBlog <ArrowIcon /></div>
+          <div className={cls.roundedBlueBtn}>
+            MediaBlog <ArrowIcon />
+          </div>
           <Text>Yuridik shaxslar uchun</Text>
           <Text>Do'konlar</Text>
           <Text>Biz bilan bog'laning!</Text>
-          {/* <Select variant='flushed' placeholder={currentLanguage} value={lang} onChange={changeLanguage}>
-            <option value="uz">O'zbek</option>
-            <option value="ru">Руский</option>
-            <option value="en">English</option>
-          </Select> */}
-          <Select
-            className="basic-single"
+          {/* <Select
+            className={`'basic-single' ${cls.language}`}
             classNamePrefix="select"
             defaultValue={langOptions[0]}
             name="language"
             options={langOptions}
             onChange={changeLanguage}
-          />
+          /> */}
+          <select value={lang} onChange={changeLanguage} className={cls.language}>
+            {langOptions.map((lang, index) => (
+              <option key={index} value={lang.value}>
+                {lang.label}
+              </option>
+            ))}
+          </select>
         </Flex>
       </Flex>
     </div>
-  )
+  );
 }
